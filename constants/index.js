6c4a095a01569a83ae41862f8aa402fe5ff7a350
oyvvl <940709@gmail.com>
2019-10-29 18:47:57 +0900

    typo fix

--- constants/index.js
+++ constants/index.js
@@ -1,6 +1,6 @@
 const localIPAddress = '192.168.0.57';
 const localClientDomain = `http://${localIPAddress}:3000`;
-const cloudClientDomain = `https://wwww.thingdex.space`;
+const cloudClientDomain = `https://www.thingdex.space`;
 
 const localServerDomain = `http://${localIPAddress}:4000`;
 const cloudServerDomain = 'https://api.thingdex.space';
